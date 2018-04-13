50111ef014227dffb60f7163cb7a2a1ae069c571
Dvakote <38066303+Dvakote@users.noreply.github.com>
2018-04-13 19:44:59 +0500

    Update N-body.py

--- N-body.py
+++ N-body.py
@@ -14,7 +14,7 @@ import numpy as np
 #import statistics as stat
 #import matplotlib as mpl
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D as p3
+#from mpl_toolkits.mplot3d import Axes3D as p3
 #from matplotlib import animation
 #===========================================================================
 # ^ Подключаемые пакеты ^
@@ -23,37 +23,42 @@ from mpl_toolkits.mplot3d import Axes3D as p3
 # Средняя масса наблюдаемых объектов и их пекулярная скорость
 #M_avg = 1.98892 * pow(10, 41) #кг
 #V_avg = 0 #4 * pow(10, 5) / np.sqrt(3) #м/с
-M_avg = pow(10, 11) #масс Солнц
-V_avg = 0 #1.3 * pow(10, -2) / np.sqrt(3) #кпк/(10^12 c)
+m_avg = pow(10, 11) #масс Солнц
+v_avg = 0 #1.3 * pow(10, -2) / np.sqrt(3) #кпк/(10^12 c)
+#M_avg = 1 #масс Млечного пути
+#V_avg = 0 #1.3 * pow(10, -2) / np.sqrt(3) #Мпк/(10^15 c)
 
 # Минимальный размер ячейки по одной оси координат
 #Distance = 2 * 3.08567758 * pow(10, 22) #м
 Distance = 5 * m.pow(10, 3) #кпк
+#Distance = 5 #Мпк
 
 # Временной интервал
 #time_step = pow(10, 13) #с
-time_step = 100 #10^12 с
+time_step = 10 #10^12 с
+#time_step = 0.01 #10^15 с
 
 # Гравитационная постоянная
 #G = 6.67408313 * m.pow(10, -11) #м^3/(кг*с^2)
 G = 4.51811511 * m.pow(10, -15) #кпк^3/(М_(Солнца)* (10^12 с)^2)
+#G = 4.51811511 * m.pow(10, -7) #кпк^3/(М_(Млечного пути)* (10^15 с)^2)
 #===========================================================================
 # ^ Константы ^
 # v Параметры системы v
 #===========================================================================
 # Задаем первоначальный размер системы в единицах "Distance"
 # для функции parameters_test
-i_test = 2
-j_test = 2
-k_test = 2
+i_test = 15
+j_test = 15
+k_test = 15
 
 # Количество ячеек по одной оси координат (для tree codes)
 # ОБЯЗАТЕЛЬНО должно быть в виде 2^(целое положительное число)
-n = 16
+n = 8
 # Количество частиц
-N = 10000
+N = 3375
 # Число шагов
-Steps = 500
+Steps = 1
 #===========================================================================
 # ^ Параметры системы ^
 # v Используемые функции v
@@ -61,14 +66,14 @@ Steps = 500
 # Подфункция, позволяющая сгенерировать определенные
 # параметры для тела
 def parameters_test(h, p, l):
-    x = Distance * (1 / 2 + h / 1)
-    y = Distance * (1 / 2 + p / 1)
-    z = Distance * (1 / 2 + l / 1)
+    x = Distance * (1 + h * 2) / 15
+    y = Distance * (1 + p * 2) / 15
+    z = Distance * (1 + l * 2) / 15
 #       Распределение скоростей и масс считаем нормальным
     Vx = 0
     Vy = 0
     Vz = 0
-    mass = abs(M_avg)
+    mass = abs(m_avg)
     Sum = np.array([x, y, z, Vx, Vy, Vz, mass, 0, 0, 0, 0])
     return Sum
 #_____________________________________________________________________________
@@ -79,10 +84,10 @@ def randomize_parameters():
     z = r.random() * n * Distance
 #   Распределение скоростей и масс считаем нормальным
 #   (пока что квадратичное отклонение выбрано наугад)
-    Vx = r.normalvariate(0, 4) * V_avg
-    Vy = r.normalvariate(0, 4) * V_avg
-    Vz = r.normalvariate(0, 4) * V_avg
-    mass = abs(r.normalvariate(M_avg, 0.5*M_avg))
+    Vx = r.normalvariate(0, 4) * v_avg
+    Vy = r.normalvariate(0, 4) * v_avg
+    Vz = r.normalvariate(0, 4) * v_avg
+    mass = abs(r.normalvariate(m_avg, 0.5*m_avg))
     Sum = np.array([x, y, z, Vx, Vy, Vz, mass, 0, 0, 0, 0])
     return Sum
 #_____________________________________________________________________________
@@ -127,7 +132,7 @@ def g_force_Newton(Particles, l, h):
     a = -G * a
     return a
 #_____________________________________________________________________________
-# Ньютоновская гравитация, прямой метод
+# Ньютоновская гравитация, метод частица-частица
 def N_body_direct(X0):
     X0[:, 3:6] += X0[:, 7:10] * time_step / 2
     X0[:, 0:3] += X0[:, 3:6] * time_step
@@ -152,14 +157,14 @@ def distribution(X0, X_size):
     return X0[X0[:, 10].argsort(kind='mergesort')]
 #_____________________________________________________________________________
 # Функция, вычисляющая параметры самых малых ячеек из параметров
-# находящихся внутри частиц (3.04.18)
+# находящихся внутри частиц (13.04.18)
 def particles_to_cell(Y, Y_size, order_n):
     n_total = int(m.pow(n, 3))
-    R_local = np.zeros([n_total, 15])
+    R_local = np.zeros([n_total, 18])
     part_num = 0
     part_count = 0
     for cell_num in range(n_total):
-        R = np.zeros([6])
+        R = np.zeros([9])
         if not part_num == Y_size:
             while Y[part_num, 10] == cell_num:
                 R[0:3] += Y[part_num, 0:3] * Y[part_num, 6]
@@ -169,33 +174,41 @@ def particles_to_cell(Y, Y_size, order_n):
                     break
         R[4] = part_count
         R[5] = part_num
+        cell_x = cell_num // (n * n)
+        R[6] = Distance * (0.5 + cell_x)
+        R[7] = Distance * (0.5 + ((cell_num // n) - cell_x * n))
+        R[8] = Distance * (0.5 + (cell_num % n))
         part_count = part_num
         if not R[3] == 0:
             R[0:3] = R[0:3] / R[3]
         R_local[cell_num] = [R[0], R[1], R[2], order_n, R[4], R[5], R[3],   \
-                0, 0, 0, 0, 0, 0, 0, 0]
+                0, 0, 0, 0, 0, 0, 0, 0, R[6], R[7], R[8]]
     return R_local
 #_____________________________________________________________________________
 # Функция, вычисляющая параметры ячеек за счет
-# находящихся внутри ячеек с меньшим порядком (5.04.18)
+# находящихся внутри ячеек с меньшим порядком (13.04.18)
 def cells_to_cell(R_final, order_n):
-    n_linear = int(m.pow(2, order_n))
-    n_total = int(m.pow(n_linear, 3))
-    R_local = np.zeros([n_total, 15])
+    cell_length = Distance * (n / order_n)
+    n_linear = order_n * 2
+    n_total = int(m.pow(order_n, 3))
+    R_local = np.zeros([n_total, 18])
     for cell_num in range(n_total):
-        R = np.zeros([4])
-        cell_x = cell_num // (n_linear * n_linear)
-        cell_y = (cell_num // n_linear) - cell_x * n_linear
-        cell_z = cell_num % n_linear
-        cell_num_0 = int(cell_x * n_linear * n_linear * 8 \
-                         + cell_y * n_linear * 4 + cell_z * 2)
-        Numbers = [cell_num_0, cell_num_0 + 1,                              \
-                   cell_num_0 + n_linear * 2,                               \
-                   cell_num_0 + n_linear * 2 + 1,                           \
-                   cell_num_0 + int(m.pow(n_linear * 2, 2)),                \
-                   cell_num_0 + int(m.pow(n_linear * 2, 2)) + 1,            \
-                   cell_num_0 + int(m.pow(n_linear * 2, 2)) + n_linear * 2, \
-                   cell_num_0 + int(m.pow(n_linear * 2, 2)) + n_linear * 2 + 1]
+        R = np.zeros([7])
+        cell_x = cell_num // (order_n * order_n)
+        cell_y = (cell_num // order_n) - cell_x * order_n
+        cell_z = cell_num % order_n
+        R[4] = cell_length * (0.5 + cell_x)
+        R[5] = cell_length * (0.5 + cell_y)
+        R[6] = cell_length * (0.5 + cell_z)
+        cell_num_0 = 2 * int(cell_x * n_linear * n_linear \
+                         + cell_y * n_linear + cell_z)
+        Numbers = [cell_num_0, cell_num_0 + 1,                      \
+                   cell_num_0 + n_linear,                           \
+                   cell_num_0 + n_linear + 1,                       \
+                   cell_num_0 + n_linear * n_linear,                \
+                   cell_num_0 + n_linear * n_linear + 1,            \
+                   cell_num_0 + n_linear * n_linear + n_linear,     \
+                   cell_num_0 + n_linear * n_linear + n_linear + 1]
         for u in range(8):
             R[0:3] += R_final[int(Numbers[u]), 0:3] \
                     * R_final[int(Numbers[u]), 6]
@@ -204,7 +217,8 @@ def cells_to_cell(R_final, order_n):
             R[0:3] = R[0:3] / R[3]
         R_local[cell_num] = [R[0], R[1], R[2], order_n, 0, 0, R[3], \
                 Numbers[0], Numbers[1], Numbers[2], Numbers[3],     \
-                Numbers[4], Numbers[5], Numbers[6], Numbers[7]]
+                Numbers[4], Numbers[5], Numbers[6], Numbers[7],     \
+                R[4], R[5], R[6]]
     return np.vstack((R_local, R_final))
 #_____________________________________________________________________________
 # Функция, рассчитывающая ускорение частицы под номером Part_num,
@@ -245,64 +259,81 @@ def int_particles_to_particle(Particles, Part_num, Mass_center, cell_num):
 # Функция, определяющая тип взаимодействия (частица-частица,
 # мультиполь-частица), а так же раскладывающая большой куб
 # на кубы меньшего размера, если удовлетворяется условие
-# раскрытия ячейки (3.04.18)
+# раскрытия ячейки (13.04.18)
 #ПРОВЕРИТЬ НА ОШИБКИ
 def calculate_cell(Particles, Mass_center,  \
-                   Part_num, order_count,   \
+                   min_cell, order_count,   \
                    L, cell_num):
-    A = np.array([0, 0, 0])
+    global n1, n2
+    A = np.zeros([n2 - n1, 3])
     if not Mass_center[cell_num, 6] == 0:
-        if part_distance(Particles, Mass_center, Part_num, cell_num) > L:
-            A = int_cell_to_particle(Particles, Mass_center,    \
-                                     Part_num, cell_num)
+        sqr_dist = (Mass_center[min_cell, 15] - Mass_center[cell_num, 15])  \
+        * (Mass_center[min_cell, 15] - Mass_center[cell_num, 15])           \
+        + m.pow(Mass_center[min_cell, 16] - Mass_center[cell_num, 16], 2)   \
+        + m.pow(Mass_center[min_cell, 17] - Mass_center[cell_num, 17], 2)
+        if sqr_dist >= (L * L):
+            for Part_num in range(n1, n2):
+                A[Part_num - n1] = int_cell_to_particle(Particles,  \
+                 Mass_center, Part_num, cell_num)
         else:
-            if Mass_center[cell_num, 8] == 0:
-                A = int_particles_to_particle(Particles, Part_num,  \
-                                              Mass_center, cell_num)
+            if Mass_center[cell_num, 3] == n:
+                for Part_num in range(n1, n2):
+                    A[Part_num - n1] = int_particles_to_particle(Particles, \
+                     Part_num, Mass_center, cell_num)
             else:
-                A = calculate_cube(Particles, Mass_center, Part_num,\
+                A = calculate_cube(Particles, Mass_center, min_cell,\
                                    order_count, cell_num)
     return A
 #_____________________________________________________________________________
 # Функция, представляющая один большой куб как 8 кубов меньшего размера
-#ПРОВЕРИТЬ НА ОШИБКИ (5.04.18)
+#ПРОВЕРИТЬ НА ОШИБКИ (13.04.18)
 def calculate_cube(Particles, Mass_center,  \
-                     Part_num, order_count, \
+                     min_cell, order_count, \
                      cell_num):
-    n_linear = int(m.pow(2, Mass_center[cell_num, 3]))
-    L = m.sqrt(3) * Distance * n / (2 * n_linear)
+    n_linear = int(Mass_center[cell_num, 3])
+    L = Distance * n / n_linear
     order_count += m.pow(n_linear, 3)
-    A = np.zeros([8, 3])
-    A[0] = calculate_cell(Particles, Mass_center, Part_num, order_count, L, \
-                                  int(order_count + Mass_center[cell_num, 7]))
-    A[1] = calculate_cell(Particles, Mass_center, Part_num, order_count, L, \
-                                  int(order_count + Mass_center[cell_num, 8]))
-    A[2] = calculate_cell(Particles, Mass_center, Part_num, order_count, L, \
-                                  int(order_count + Mass_center[cell_num, 9]))
-    A[3] = calculate_cell(Particles, Mass_center, Part_num, order_count, L, \
-                                  int(order_count + Mass_center[cell_num, 10]))
-    A[4] = calculate_cell(Particles, Mass_center, Part_num, order_count, L, \
-                                  int(order_count + Mass_center[cell_num, 11]))
-    A[5] = calculate_cell(Particles, Mass_center, Part_num, order_count, L, \
-                                  int(order_count + Mass_center[cell_num, 12]))
-    A[6] = calculate_cell(Particles, Mass_center, Part_num, order_count, L, \
-                                  int(order_count + Mass_center[cell_num, 13]))
-    A[7] = calculate_cell(Particles, Mass_center, Part_num, order_count, L, \
-                                  int(order_count + Mass_center[cell_num, 14]))
+    A = np.zeros([8, n2 - n1, 3])
+    A[0] = calculate_cell(Particles, Mass_center, min_cell, order_count,\
+                     L, int(order_count + Mass_center[cell_num, 7]))
+    A[1] = calculate_cell(Particles, Mass_center, min_cell, order_count,\
+                     L, int(order_count + Mass_center[cell_num, 8]))
+    A[2] = calculate_cell(Particles, Mass_center, min_cell, order_count,    \
+                     L, int(order_count + Mass_center[cell_num, 9]))
+    A[3] = calculate_cell(Particles, Mass_center, min_cell, order_count,\
+                     L, int(order_count + Mass_center[cell_num, 10]))
+    A[4] = calculate_cell(Particles, Mass_center, min_cell, order_count,\
+                     L, int(order_count + Mass_center[cell_num, 11]))
+    A[5] = calculate_cell(Particles, Mass_center, min_cell, order_count,\
+                     L, int(order_count + Mass_center[cell_num, 12]))
+    A[6] = calculate_cell(Particles, Mass_center, min_cell, order_count,\
+                     L, int(order_count + Mass_center[cell_num, 13]))
+    A[7] = calculate_cell(Particles, Mass_center, min_cell, order_count,\
+                     L, int(order_count + Mass_center[cell_num, 14]))
     return A.sum(axis=0)
 #_____________________________________________________________________________
-# Функция, подщитывающая ускорение для каждой частицы (3.04.18)
+# Функция, подщитывающая ускорение для каждой частицы (13.04.18)
 def interaction(Particles, Mass_center):
-    Number_of_particles = np.size(Particles, 0)
-    for Part_num in range(Number_of_particles):
-        Particles[Part_num, 7:10] = G * calculate_cube(Particles, Mass_center,\
-                     Part_num, 0, 0)
+    global n1, n2
+    cell_linear = 1
+    cell_count = 0
+    max_cell_linear = Mass_center[-1, 3] / 2
+    while cell_linear <= max_cell_linear:
+        cell_count += int(m.pow(cell_linear, 3))
+        cell_linear *= 2
+    for min_cell in range(cell_count, int(np.size(Mass_center, 0))):
+        n1 = int(Mass_center[min_cell, 4])
+        n2 = int(Mass_center[min_cell, 5])
+        if not n1 == n2:
+            Particles[n1:n2, 7:10] = calculate_cube(Particles, Mass_center, \
+                                                 min_cell, 0, 0)
+    Particles[:, 7:10] *= G
     return Particles
 #_____________________________________________________________________________
 # Функция, позволяющая получить новые параметры частиц
-# из матрицы Y с помощью метода Tree code
+# из матрицы Y с помощью метода Tree code (13.04.18)
 def tree_code_gravity(Y):
-    order_n = int(m.log2(n))
+    order_n = n
     Y_size = np.size(Y, 0)
 #    start = time.time()
     Y = distribution(Y, Y_size)
@@ -312,8 +343,8 @@ def tree_code_gravity(Y):
     Y[:, 0:3] += Y[:, 3:6] * time_step
 #    start = time.time()
     R_final = particles_to_cell(Y, Y_size, order_n)
-    while order_n > 0:
-        order_n += -1
+    while order_n > 1:
+        order_n *= 0.5
         R_final = cells_to_cell(R_final, order_n)
 #    computing_time = time.time() - start
 #    print("Работа с ячейками", computing_time, "с")
@@ -325,18 +356,14 @@ def tree_code_gravity(Y):
     return Y
 #_____________________________________________________________________________
 # Функция для "скирншота" положения всех частиц
-def screenshot(System_parameters, Name):
+def screenshot(System_parameters, Name, point_size):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     x = System_parameters[:, 0]
     y = System_parameters[:, 1]
     z = System_parameters[:, 2]
-    ax.scatter(x, y, z, color='red', s=0.02)
+    ax.scatter(x, y, z, color='red', s=point_size)
     ax.autoscale(False)
-#    Для осей в единицах СИ
-#    ax.set_xlabel('x, м')
-#    ax.set_ylabel('y, м')
-#    ax.set_zlabel('z, м')
     ax.set_xlabel('x, кпк')
     ax.set_ylabel('y, кпк')
     ax.set_zlabel('z, кпк')
@@ -346,18 +373,23 @@ def screenshot(System_parameters, Name):
 # ^ Используемые функции ^
 # v Область с исполняемым кодом v
 #===========================================================================
+n1 = 0
+n2 = 0
+name_begin = "Начальное положение.png"
+name_end = "Итоговое положение.png"
+marker_size = 0.02
 #X = birth_test()
 X = birth_random(N)
-Name = "Начальное положение.png"
-screenshot(X, Name)
+#screenshot(X, Name_begin, marker_size)
 start = time.time()
 for q in range(Steps):
     X = tree_code_gravity(X)
 #    X1 = N_body_direct(X)
-#    if q in [50, 100, 150, 200, 250, 300, 350, 400, 450]:
+#    if q in [250, 500, 750, 1000, 1250]:
 #        screenshot(X, 'Шаг' + str(q))
 #print(X)
 computing_time = time.time() - start
-print("Полное время вычислений", computing_time, "с")
-Name = "Итоговое положение.png"
-screenshot(X, Name)
+print("Время выполнения", computing_time, "с")
+#screenshot(X, Name_end)
+#===========================================================================
+# ^ Область с исполняемым кодом ^
